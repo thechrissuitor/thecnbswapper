283326b3d536a101f6f71b4aa9ce51d8d7d58784
Nana Owusu Nimako <nana.nimako@uvm.edu>
2018-11-15 12:11:18 -0500

    all thats done so far

--- trinity.php
+++ trinity.php
@@ -6,6 +6,18 @@ include "top.php";
         <h2>Trinity Campus</h2>
         <p>Ben, you'll populate these pages. Nana, you'll style them.</p>
     </article>
+
+    <article class="row">
+		<figure class="card col-sm-3" id="smallC">
+	  		<img class="card-img-top" src="images/mcAuley_outside.JPG" alt="Card image cap">
+	  		<figcaption class="container text-center">McAuley Hall</figcaption>
+		</figure>
+
+		<figure class="card col-sm-3" id="smallC">
+	  		<img class="card-img-top" src="images/mercy_outside.JPG" alt="Card image cap">
+	  		<figcaption class="container text-center">Mercy Hall</figcaption>
+		</figure>
+	</article>
 </main>
 
 <?php include "footer.php"; ?>
