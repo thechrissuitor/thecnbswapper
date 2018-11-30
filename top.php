224418835ea4d29e9721dfd8e4b7783c9b43bc58
Christopher Thomas Suitor <christopher.suitor@uvm.edu>
2018-11-30 14:20:23 -0500

    added Nana's links

--- top.php
+++ top.php
@@ -17,6 +17,7 @@ $path_parts = pathinfo($phpSelf);
         <![endif]-->
 
         <!-- Bootstrap  Link -->
+        <!-- Bootstrap Received from: https://getbootstrap.com/docs/4.1/getting-started/introduction/ -->
 		<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.1/css/bootstrap.min.css" integrity="sha384-WskhaSGFgHYWDcbwN70/dfYBj47jz9qbsMId/iRN3ewGhXQFZCSftd1LZCfmhktB" crossorigin="anonymous">
 
 		<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
