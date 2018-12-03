48773489604065c87d160884fd7c515cbd44be5f
Nana Owusu Nimako <nana.nimako@uvm.edu>
2018-12-03 09:37:41 -0500

    recent

--- header.php
+++
@@ -1,4 +0,0 @@
-<!-- ######################     Page header   ############################## -->
-<header>
-    <h1 class="container text-center CNB">The CNB Swapper</h1>
-</header>
--- redstone.php
+++ redstone.php
@@ -8,22 +8,22 @@ include "top.php";
 
     <article class="row campus">
 		<figure class="card smallC" id="">
-            <img class="card-img-top" src="images/HarrisMillis.png" alt="Card image cap">
+                    <img class="card-img-top" src="images/coolidge.jpg" alt="Card image cap">
 	  		<figcaption class="container text-center">Collidge Hall</figcaption>
 		</figure>
 
 		<figure class="card smallC" id="">
-            <img class="card-img-top" src="images/UHeightsNorth.png" alt="Card image cap">
+                    <img class="card-img-top" src="images/davis.jpg" alt="Card image cap">
 	  		<figcaption class="container text-center">Davis Hall</figcaption>
 		</figure>
         
         <figure class="card smallC" id="">
-            <img class="card-img-top" src="images/UHeightsSouth.png" alt="Card image cap">
+            <img class="card-img-top" src="images/mason.jpg" alt="Card image cap">
 	  		<figcaption class="container text-center">Mason Hall</figcaption>
 		</figure>
         
         <figure class="card smallC" id="">
-            <img class="card-img-top" src="images/LL.png" alt="Card image cap">
+            <img class="card-img-top" src="images/patterson.jpg" alt="Card image cap">
 	  		<figcaption class="container text-center">Patterson Hall</figcaption>
 		</figure>
         
@@ -33,29 +33,24 @@ include "top.php";
 		</figure>
 
         <figure class="card smallC" id="">
-            <img class="card-img-top" src="images/richardson.JPG" alt="Card image cap">
+            <img class="card-img-top" src="images/simpson" alt="Card image cap">
             <figcaption class="container text-center">Simpson Hall</figcaption>
 		</figure>
 
         <figure class="card smallC" id="">
-            <img class="card-img-top" src="images/ready.JPG" alt="Card image cap">
+            <img class="card-img-top" src="images/wilks.jpg" alt="Card image cap">
             <figcaption class="container text-center">Wilks Hall</figcaption>
 		</figure>
         
         <figure class="card smallC" id="">
-            <img class="card-img-top" src="images/ready.JPG" alt="Card image cap">
+            <img class="card-img-top" src="images/wings.jpg" alt="Card image cap">
             <figcaption class="container text-center">Wing Hall</figcaption>
-		</figure>
+        </figure>
 
         <figure class="card smallC" id="">
-            <img class="card-img-top" src="images/ready.JPG" alt="Card image cap">
+            <img class="card-img-top" src="images/wright.jpg" alt="Card image cap">
             <figcaption class="container text-center">Wright Hall</figcaption>
-		</figure>
-
-<!--         <figure class="card smallC" id="">
-            <img class="card-img-top" src="images/ready.JPG" alt="Card image cap">
-            <figcaption class="container text-center">Wilks Hall</figcaption>
-		</figure> --> 
+        </figure>
 	</article>
 </main>
 
